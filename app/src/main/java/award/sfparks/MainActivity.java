0333e4a1e922a26df08d3db1b3b3c63c7c209b28
Adam Ward <AdamWardVGP@gmail.com>
2016-02-22 13:18:25 -0800

    Inport Cleanup

--- app/src/main/java/award/sfparks/MainActivity.java
+++ app/src/main/java/award/sfparks/MainActivity.java
@@ -2,15 +2,6 @@ package award.sfparks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import java.util.List;
-
-import javax.inject.Inject;
-
-import award.sfparks.data.ParksService;
-import award.sfparks.model.ParkInfo;
-import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
 
